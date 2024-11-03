8f34c2ab841a9922f2303614e972625f38d7afe3
mayur <mayur.thool@viklele.com>
2024-11-03 18:14:21 +0530

    Minor Bug Fixes

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
     statusCode,
     message = "Something went wrong",
     errors = [],
-    stack
+    stack = ""
   ) {
     super(message);
 
@@ -20,3 +20,5 @@ class ApiError extends Error {
     }
   }
 }
+
+export { ApiError };
