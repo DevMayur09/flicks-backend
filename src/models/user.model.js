daa64997ac10eddb94fb568b74bdb5dd07a66497
mayur <mayur.thool@viklele.com>
2024-11-06 00:53:40 +0530

    Bug Fixed and minor changes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -90,9 +90,11 @@ userSchema.methods.generateRefreshToken = function () {
     },
     process.env.REFRESH_TOKEN_SECRET,
     {
-      expiresIn: process.env.REFRESH_TOKEN_SECRET,
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
     }
   );
+
+  return token;
 };
 
 export const User = mongoose.model("User", userSchema);
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -3,7 +3,7 @@ class ApiResponse {
     this.data = data;
     this.message = message;
     this.success = statusCode < 400;
-    this.statusCode = statusCode < 400;
+    this.statusCode = statusCode;
   }
 }
 
